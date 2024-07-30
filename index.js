ba00eb1984cf3b30c1318e18e9a17d38c03c082f
Ronei Couto <ronei@roneicouto.com>
2024-07-29 22:32:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const pgp = require("pg-promise")()
+const db = pgp("postgresql://admin:mIK9DvSdNM0UFMHwTx0UBz2uQyLeIgnm@dpg-cqk3i1jqf0us73c29190-a/api_hy0l");
+db.query("SELECT 1 + 1 as RESULT;").then((result) => console.log(resultado));
 
+module.exports = db;
